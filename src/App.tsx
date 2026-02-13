9bc1287413c51fccf2f8c382ed600dfe18567230
Gerardo Venegas <gmatiasvc@hotmail.com>
2026-02-13 11:22:51 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+const Navbar = () => {
+  return(
+
+    <nav className="h-96">
+      <img src=""></img>
+    </nav>
+
+  );
+}
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
